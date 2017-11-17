7e29028c93ce5ffcd3decf72101739018254f9b8
Adam Titus <adamtitus76@gmail.com>
2017-11-17 11:45:16 -0800

    successfully test date comparison in seconds.

--- js/Age.js
+++ js/Age.js
@@ -1,10 +1,14 @@
 export class Age {
-  consructor(dob) {
-    this.birthdate = dob;
+  consructor() {
+    this.secondsPerDay = 86400;
+    this.secondsPerMonth = 2.628e+6;
+    this.secondsPerYear = 3.154e+7;
   }
-  ageChecker() {
+  ageChecker(date) {
     const today = new Date();
-    const result = today - this.birthdate
-    return(result);
+    let todaysSeconds = today.getTime();
+    let datesSeconds = date.getTime();
+    let totalSeconds = (todaysSeconds - datesSeconds)/1000;
+    return(totalSeconds);
   }
 }
--- spec/Age-spec.js
+++ spec/Age-spec.js
@@ -2,10 +2,9 @@ import { Age } from "./../js/Age.js";
 
 describe('ageChecker', function(){
   it('should return 3 numbers', function(){
-    const newDate = new Date();
-    const dateChange = newDate.setFullYear(2016, 9, 16);
-    const newAge = new Age(dateChange);
-    const expected = 111;
-    expect(newAge.ageChecker()).toEqual(expected);
+    const newDate = new Date(2017, 10, 17);
+    const newAge = new Age();
+    const expected = 0;
+    expect(newAge.ageChecker(newDate)).not.toBeLessThan(expected);
   });
 });
