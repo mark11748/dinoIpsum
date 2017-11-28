c9b87ef951e6495fa9078099b6240c9921f0ca24
James Osborn and mark woodward <james+mark@>
2017-11-28 14:11:52 -0800

    add img representation of status; add responsiveness to feed btn-stat changes on click

--- build/js/app.js
+++ build/js/app.js
@@ -61,6 +61,26 @@ var _tamagotchi = require("./../js/tamagotchi.js");
 function updateTimer(thing) {
   var clock = setInterval(function () {
     $("h2#LOG").text("Countdown: " + thing.timer);
+    switch (true) {
+      case thing.timer >= 20:
+        $("div.images").empty();
+        $("div.images").append("<img src=\"./root/img/happy.png\">");
+        break;
+      case thing.timer < 20 && thing.timer >= 15:
+        $("div.images").empty();
+        $("div.images").append("<img src=\"./root/img/neutral.png\">");
+        break;
+      case thing.timer < 15 && thing.timer >= 10:
+        $("div.images").empty();
+        $("div.images").append("<img src=\"./root/img/frowny.png\">");
+        break;
+      case thing.timer < 10:
+        $("div.images").empty();
+        $("div.images").append("<img src=\"./root/img/Awright_Facebones!.jpg\">");
+        break;
+      default:
+        break;
+    }
   }, 100);
 };
 
--- js/tamagotchi-interface.js
+++ js/tamagotchi-interface.js
@@ -4,6 +4,27 @@ import { Tamagotchi } from './../js/tamagotchi.js';
 function updateTimer(thing) {
   const clock = setInterval(() => {
     $("h2#LOG").text(`Countdown: ${thing.timer}`);
+    switch (true)
+    {
+      case (thing.timer>=20):
+        $("div.images").empty();
+        $("div.images").append("<img src=\"./root/img/happy.png\">");
+      break;
+      case (thing.timer<20 && thing.timer>=15):
+        $("div.images").empty();
+        $("div.images").append("<img src=\"./root/img/neutral.png\">");
+      break;
+      case (thing.timer<15 && thing.timer>=10):
+        $("div.images").empty();
+        $("div.images").append("<img src=\"./root/img/frowny.png\">");
+      break;
+      case (thing.timer<10):
+        $("div.images").empty();
+        $("div.images").append("<img src=\"./root/img/Awright_Facebones!.jpg\">");
+      break;
+      default:
+      break;
+    }
   },100);
 };
 
